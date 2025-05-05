4ac5884fd6c63419167a7b546ca291c06837b0ee
TheEinfachFelix <the.einfach.felix@gmail.com>
2025-05-05 12:47:54 +0200

    with gitignore

--- 4-Gewinnt/4-Gewinnt/obj/Debug/net8.0-windows/4-Gewinnt.AssemblyInfo.cs
+++ 4-Gewinnt/4-Gewinnt/obj/Debug/net8.0-windows/4-Gewinnt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-Gewinnt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfa3447d9a911ef93fcec49258213335ef77fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a84ebce67f196945fd3a50e269ddb263ebd79da")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-Gewinnt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-Gewinnt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
