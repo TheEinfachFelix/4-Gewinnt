930aa2b6db1b48079d000dce4c5a5426ec24bbe2
TheEinfachFelix <the.einfach.felix@gmail.com>
2025-05-19 10:56:52 +0200

    done

--- 4-Gewinnt/4-Gewinnt-Logik/obj/Debug/net8.0/4-Gewinnt-Logik.AssemblyInfo.cs
+++ 4-Gewinnt/4-Gewinnt-Logik/obj/Debug/net8.0/4-Gewinnt-Logik.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-Gewinnt-Logik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fcfbaebf2f11b632b0b248fe5a4ede3c7720cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872925b91387699f9634034c22a9c0d0163c67a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-Gewinnt-Logik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-Gewinnt-Logik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
